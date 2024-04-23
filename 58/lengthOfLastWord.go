a2e32bda88c933622f636ab2c7c1cca54d945dfa
Akhil Das <akhilpic1@gmail.com>
2024-04-23 06:34:32 +0530

    58 Length of the last word

---
+++ 58/lengthOfLastWord.go
@@ -0,0 +1,13 @@
+package main
+
+import "strings"
+
+func lengthOfLastWord(s string) int {
+	trimmed := strings.TrimSpace(s)
+
+	words := strings.Split(trimmed, " ")
+
+	lastWord := words[len(words)-1]
+	return len(lastWord)
+
+}
